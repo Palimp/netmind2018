6013afb7f92e17f45cf018f60159149708dd9933
Fernando <fernando.pincay@gmail.com>
2018-08-30 11:36:16 +0200

    Agregada function calcularIva

--- jp.php
+++ jp.php
@@ -8,3 +8,10 @@ function calcularIVA(){
     $precio=1000;
     return $precio*$iva;
 }
+
+// Funcion chachiPiruli
+function chachiPiruli($name) {
+    $chachi = 'Hola $name eres muy Chachi Piruli';
+    
+    return $chachi;
+}
